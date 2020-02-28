3ff151431c0ae93b8edd7eb39326b269cb0b6bb4
charles951008 <charles951008@163.com>
2020-02-29 00:01:28 +0800

    helloRabbitMQ

---
+++ src/main/java/com/wsl/rabbitmq/workfair/Recv2.java
@@ -0,0 +1,53 @@
+package com.wsl.rabbitmq.workfair;
+
+import com.rabbitmq.client.*;
+import com.wsl.rabbitmq.util.ConnectionUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv2 {
+    private static final String QUEUE_NAME = "test_work_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //获取链接
+        Connection connection = ConnectionUtils.getConnection();
+
+        //获取channel
+         final Channel channel = connection.createChannel();
+
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+
+        int prefetchCount=1;
+        channel.basicQos(prefetchCount);//保证一次只分发一个
+
+
+        //定义一个消费者
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            //一旦有消息就会触发这个方法  消息到达
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                String msg = new String(body, "utf-8");
+                System.out.println("[2] Recv msg:" + msg);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("[2] done");
+                    //手动回执一个消息
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+                }
+            }
+        };
+
+        boolean autoAck = false;
+        channel.basicConsume(QUEUE_NAME, autoAck, defaultConsumer);
+    }
+}
